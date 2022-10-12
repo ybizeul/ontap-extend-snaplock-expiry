232a5a023ca06b2962889bc0c1f4564e86cc829f
Yann Bizeul <yann@tynsoe.org>
2022-10-12 19:07:02 +0200

    Code documentation

--- ontap-extend-snaplock-expiry.py
+++ ontap-extend-snaplock-expiry.py
@@ -10,6 +10,7 @@ import urllib3
 
 version = 0.9
 
+# Arguments Parsing
 parser = argparse.ArgumentParser(description='Update Snaplock snapshot expiry time according to snapmirror labels')
 parser.add_argument('--version', '-v', action='version', version='%(prog)s ' + str(version))
 parser.add_argument('--simulate', '-s', dest="simulate", action="store_true", default=False, help="Simulate, don't apply expiry date change and report on what would be done")
@@ -17,34 +18,42 @@ parser.add_argument('-k', dest="ignore_ssl", action="store_true", default=False,
 
 args = parser.parse_args()
 
+# Notify if we're running in simulate mode
 if args.simulate:
     print("Running in simulate mode")
 
+# If -k is used, ignore SSL warnings
 if args.ignore_ssl:
     urllib3.disable_warnings()
 
+# Read configuration file
 with open('config.json','r') as configfile:
     config = json.load(configfile)
 
 snapmirror_labels = config['labels-policies'].keys()
 
+# Start connecting to configured systems
 for system in config["systems"]:
     auth = (system["username"],system["password"])
     try:
         r = requests.get('https://%s/api/storage/volumes' % system["ip"], auth=auth, verify=not args.ignore_ssl)
     except requests.exceptions.SSLError:
+        # Handle SSL exception
         print("Certificate verification failed for %s. Use -k or add appropriate CA to system configuration" % system["ip"])
         continue
     except requests.exceptions.ConnectionError as e:
+        # Handle other connection errors
         print("Unable to connect to %s" % system["ip"])
         print(e)
         continue
 
+    # Bail if for some reason we don't get code 200
     if r.status_code != 200:
         exit("Failed to connect to %s" % system["ip"])
 
     volumes = r.json()
 
+    # Check all volumes in the systems for snaplock snapshots
     for volume in volumes['records']:
         volume_uuid = volume['uuid']
 
@@ -55,6 +64,7 @@ for system in config["systems"]:
         
         snapshots = s.json()
 
+        # Check all snapshots in the volume
         for snapshot in snapshots['records']:
             snapshot_uuid = snapshot['uuid']
 
@@ -73,13 +83,18 @@ for system in config["systems"]:
             snapshot_snapmirror_label = 'snapmirror_label' in snapshot_details and snapshot_details['snapmirror_label'] or None
             snapshot_snaplock_expiry_time = 'snaplock_expiry_time' in snapshot_details and snapshot_details['snaplock_expiry_time'] or None
             
+            # Check if there is a snaplock expiry time and the snapmirror labl is in the configuration
             if snapshot_snapmirror_label in snapmirror_labels and snapshot_snaplock_expiry_time:
-                # We have a policy for this label
-
+            
                 # Convert create time to standard object
+                # ONTAP dates are *almost* standard, it seems it uses [+/-]HH:MM instead of [+/-]HHMM for timezone specification
                 standard_snapshot_create_time = re.sub(r'(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}[+-]\d{2}):(\d{2})',r'\1\2',snapshot_create_time)
                 snapshot_create_time_obj = datetime.datetime.strptime(standard_snapshot_create_time, '%Y-%m-%dT%H:%M:%S%z')
+
+                # Add the desired amount of seconds to create_time to determine snaplock_expiry_time
                 snaplock_expiry_time_obj = snapshot_create_time_obj + datetime.timedelta(seconds=config['labels-policies'][snapshot_snapmirror_label])
+
+                # Convert date back to ONTAP format
                 standard_snaplock_expiry_time=datetime.datetime.strftime(snaplock_expiry_time_obj,'%Y-%m-%dT%H:%M:%S%z')
                 snaplock_expiry_time = re.sub(r'(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}[+-]\d{2})(\d{2})',r'\1:\2',standard_snaplock_expiry_time)
 
